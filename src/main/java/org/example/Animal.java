7e2c23e92b4071851c20f58c34913a0a2db68cad
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-03 14:02:58 -0500

    Feat:Iniciando Clase Animal

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,5 +1,37 @@
 package org.example;
 
 public class Animal {
+    private String nombre;
+    private String genero;
+    private String tipo;
 
+    public Animal(String nombre, String genero, String tipo) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
