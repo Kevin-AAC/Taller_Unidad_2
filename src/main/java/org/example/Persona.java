fc05713ad4bf6812093ce3ce0635631331215a9e
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-03 14:09:25 -0500

    Feat:Atributos y Metodos Clase Persona

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,5 +1,67 @@
 package org.example;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private int sueldoHora;
+    private String cargo;
 
+    public Persona(String nombre, String cargo, int sueldoHora, String genero, int edad, String apellido) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.sueldoHora = sueldoHora;
+        this.genero = genero;
+        this.edad = edad;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getSueldoHora() {
+        return sueldoHora;
+    }
+
+    public void setSueldoHora(int sueldoHora) {
+        this.sueldoHora = sueldoHora;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
