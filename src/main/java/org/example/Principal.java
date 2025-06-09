53023d78e71bcc8fe7e20cba4fcd5e145a638960
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-09 07:39:39 -0500

    Feat:Atributos clase principal

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Principal {
+    private Map<String, List<Animal>> clasificacion;
+    private List<Animal> animales;
+    public Principal() {
+        clasificacion = new HashMap<>();
+        animales = new ArrayList<>();
+    }
+
+
 }
