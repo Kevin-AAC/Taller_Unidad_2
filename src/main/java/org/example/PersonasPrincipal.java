05c0216050137168ad805ba896c20fcf60c7d9c4
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-09 23:25:50 -0500

    Refactor:Uso de Bucle para ingresar personas

--- src/main/java/org/example/PersonasPrincipal.java
+++ src/main/java/org/example/PersonasPrincipal.java
@@ -5,28 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PersonasPrincipal {
-
+    Scanner scanner = new Scanner(System.in);
     private List<Persona> personas = new ArrayList<>();
 
 
     public void AgregarPersona() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un Nombre: ");
-        String nombre = scanner.nextLine();
-        System.out.println("Ingrese un Apellido: ");
-        String apellido = scanner.nextLine();
-        System.out.println("Ingrese un Edad: ");
-        int edad = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Ingrese un Genero: ");
-        String genero = scanner.nextLine();
-        System.out.println("Sueldo por Hora:");
-        int sueldo_hora = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Cargo:");
-        String cargo = scanner.nextLine();
-        personas.add(new Persona(nombre,apellido,edad,genero,sueldo_hora, cargo));
-        System.out.println("✅ Persona agregada correctamente.\n");
+
+        System.out.println("============================================================");
+        System.out.println("Registro de Personas");
+        System.out.println("Escriba salir en nombre para terminar los registros ");
+        System.out.println("************************************************************");
+        while (true){
+            System.out.println("Ingrese un Nombre: ");
+            String nombre = scanner.nextLine();
+            if (nombre.equalsIgnoreCase("salir")) break;
+            System.out.println("Ingrese un Apellido: ");
+            String apellido = scanner.nextLine();
+            System.out.println("Ingrese un Edad: ");
+            int edad = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese un Genero: ");
+            String genero = scanner.nextLine();
+            System.out.println("Sueldo por Hora:");
+            int sueldo_hora = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Cargo:");
+            String cargo = scanner.nextLine();
+            personas.add(new Persona(nombre,apellido,edad,genero,sueldo_hora, cargo));
+            System.out.println("✅ Persona agregada correctamente.\n");
+        }
+
+
 
     }
     public void MostrarPersonas() {
