f1e7225e27ab43468cbfa6ca0d011a288dc610c7
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-09 23:14:01 -0500

    Fix:Salto de linea en consola

--- src/main/java/org/example/PersonasPrincipal.java
+++ src/main/java/org/example/PersonasPrincipal.java
@@ -16,10 +16,12 @@ public class PersonasPrincipal {
         String apellido = scanner.nextLine();
         System.out.println("Ingrese un Edad: ");
         int edad = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Ingrese un Genero: ");
         String genero = scanner.nextLine();
         System.out.println("Sueldo por Hora:");
         int sueldo_hora = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Cargo:");
         String cargo = scanner.nextLine();
         personas.add(new Persona(nombre,apellido,edad,genero,sueldo_hora, cargo));
