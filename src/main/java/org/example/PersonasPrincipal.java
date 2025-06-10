796eda65729164a1e84fc4160b350292bccf1dcf
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-06-09 23:08:43 -0500

    Feat :Metodo Agregar Persona

--- src/main/java/org/example/PersonasPrincipal.java
+++ src/main/java/org/example/PersonasPrincipal.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PersonasPrincipal {
+    Scanner scanner = new Scanner(System.in);
+    private List<Persona> personas = new ArrayList<>();
+
+
+    public void AgregarPersona() {
+        System.out.println("Ingrese un Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese un Apellido: ");
+        String apellido = scanner.nextLine();
+        System.out.println("Ingrese un Edad: ");
+        int edad = scanner.nextInt();
+        System.out.println("Ingrese un Genero: ");
+        String genero = scanner.nextLine();
+        System.out.println("Sueldo por Hora:");
+        int sueldo_hora = scanner.nextInt();
+        System.out.println("Cargo:");
+        String cargo = scanner.nextLine();
+        personas.add(new Persona(nombre,apellido,edad,genero,sueldo_hora, cargo));
+        System.out.println("✅ Persona agregada correctamente.\n");
+
+    }
+
 }
